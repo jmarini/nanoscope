f37e40805d7ebd0cb3d51af3c7d97c1f0de66075
Jonathan Marini <j.marini@ieee.org>
2014-10-25 01:06:33 -0400

    Added tests for parsing and fixed some io issues.

--- nanoscope/nanoscope.py
+++ nanoscope/nanoscope.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals
 
+import io
+
 import numpy as np
 
 from .parameter import parse_parameter
@@ -11,8 +13,9 @@ class NanoscopeParser(object):
     Handles reading and parsing Nanoscope files.
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, encoding='cp1252'):
         self.filename = filename
+        self.encoding = encoding
         self.images = {}
         self.config = {}
 
@@ -46,30 +49,31 @@ class NanoscopeParser(object):
             self._read_image_data(image)
 
     def _read_header(self):
-        with open(self.filename, 'r') as f:
+        with io.open(self.filename, 'r', encoding=self.encoding) as f:
             for line in f:
                 parameter = parse_parameter(line.rstrip('\n'))
+                self._handle_parameter(parameter, f)
 
-                if parameter.type == 'H':  # header
-                    if parameter.header == 'File list end':
-                        return
-                    elif parameter.header == 'Ciao image list':
-                        self._read_image_header(f)
+    def _handle_parameter(self, parameter, f):
+        if parameter.type == 'H':  # header
+            if parameter.header == 'File list end':
+                return
+            elif parameter.header == 'Ciao image list':
+                self._handle_parameter(self._read_image_header(f), f)
+        elif parameter.type != 'S':
+            self.config[parameter.parameter] = parameter.hard_value
 
     def _read_image_header(self, f):
         image_config = {}
-        position = f.tell()
         for line in f:
             parameter = parse_parameter(line.rstrip('\n'))
             if parameter.type == 'H':
-                f.seek(position)
-                return
+                return parameter
             if parameter.type == 'S':
-                if parameter.param == 'Image Data':
+                if parameter.parameter == 'Image Data':
                     self.images[parameter.internal_designation] = image_config
             else:
-                image_config[parameter.param] = parameter.hard_value
-            position = f.tell()
+                image_config[parameter.parameter] = parameter.hard_value
 
     def _read_image_data(self, config):
         pass
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,168 @@
 # -*- coding: utf-8 -*-
+import datetime
 import unittest
 
 from nanoscope.nanoscope import NanoscopeParser
 
 
 class TestNanoscopeParser(unittest.TestCase):
-    pass
+
+    def test_read_header_single_section(self):
+        data = {
+            'Version': '0x05120130',
+            'Date': datetime.datetime(2014, 10, 17, 10, 27, 26),
+            'Start context': 'OL2',
+            'Data length': 40960,
+            'Text': None,
+            'History': None,
+            'Navigator note': None,
+            'Engage X Pos': -19783.4,
+            'Engage Y Pos': -42151.3,
+        }
+        p = NanoscopeParser('./tests/files/header_single_section.txt', 'utf-8')
+        p.read_nanoscope()
+        self.assertDictEqual(data, p.config)
+
+    def test_read_header_multiple_sections(self):
+        data = {
+            'Version': '0x05120130',
+            'Date': datetime.datetime(2014, 10, 17, 10, 27, 26),
+            'Start context': 'OL2',
+            'Data length': 40960,
+            'Text': None,
+            'History': None,
+            'Navigator note': None,
+            'Engage X Pos': -19783.4,
+            'Engage Y Pos': -42151.3,
+            'Description': 'D3100 NSIV',
+            'Controller': 'IV',
+            'Microscope': 'D3100',
+            'Extender': 'Quadrex',
+            'Tip Exchange': None,
+            'Vision': 'FrameGrabber',
+            'Zoom System': 'Motorized',
+            'Scanner file': '1965g.scn',
+            'Profile name': 'default',
+        }
+        p = NanoscopeParser('./tests/files/header_multiple_sections.txt', 'utf-8')
+        p.read_nanoscope()
+        self.assertDictEqual(data, p.config)
+
+    def test_read_header_single_image(self):
+        data = {
+            'Version': '0x05120130',
+            'Date': datetime.datetime(2014, 10, 17, 10, 27, 26),
+            'Start context': 'OL2',
+            'Data length': 40960,
+            'Text': None,
+            'History': None,
+            'Navigator note': None,
+            'Engage X Pos': -19783.4,
+            'Engage Y Pos': -42151.3,
+        }
+        images = {
+            'Height': {
+                'Data offset': 40960,
+                'Data length': 524288,
+                'Bytes/pixel': 2,
+                'Start context': 'OL',
+                'Data type': 'AFM',
+                'Note': None,
+                'Samps/line': 512,
+                'Number of lines': 512,
+                'Aspect ratio': '1:1',
+                'Line direction': 'Retrace',
+                'Highpass': 0,
+                'Lowpass': 0,
+                'Realtime planefit': 'Line',
+                'Offline planefit': None,
+                'Valid data start X': 0,
+                'Valid data start Y': 0,
+                'Valid data len X': 512,
+                'Valid data len Y': 512,
+                'Tip x width correction factor': 1,
+                'Tip y width correction factor': 1,
+                'Tip x width correction factor sigma': 1,
+                'Tip y width correction factor sigma': 1,
+                'Z magnify': 0.002639945,
+                'Z scale': 438.6572,
+                'Z offset': 0,
+            }
+        }
+        p = NanoscopeParser('./tests/files/header_single_image.txt', 'utf-8')
+        p.read_nanoscope()
+        self.assertDictEqual(data, p.config)
+        self.assertDictEqual(images, p.images)
+
+    def test_read_header_multiple_images(self):
+        data = {
+            'Version': '0x05120130',
+            'Date': datetime.datetime(2014, 10, 17, 10, 27, 26),
+            'Start context': 'OL2',
+            'Data length': 40960,
+            'Text': None,
+            'History': None,
+            'Navigator note': None,
+            'Engage X Pos': -19783.4,
+            'Engage Y Pos': -42151.3,
+        }
+        images = {
+            'Height': {
+                'Data offset': 40960,
+                'Data length': 524288,
+                'Bytes/pixel': 2,
+                'Start context': 'OL',
+                'Data type': 'AFM',
+                'Note': None,
+                'Samps/line': 512,
+                'Number of lines': 512,
+                'Aspect ratio': '1:1',
+                'Line direction': 'Retrace',
+                'Highpass': 0,
+                'Lowpass': 0,
+                'Realtime planefit': 'Line',
+                'Offline planefit': None,
+                'Valid data start X': 0,
+                'Valid data start Y': 0,
+                'Valid data len X': 512,
+                'Valid data len Y': 512,
+                'Tip x width correction factor': 1,
+                'Tip y width correction factor': 1,
+                'Tip x width correction factor sigma': 1,
+                'Tip y width correction factor sigma': 1,
+                'Z magnify': 0.002639945,
+                'Z scale': 438.6572,
+                'Z offset': 0,
+            },
+            'Amplitude': {
+                'Data offset': 565248,
+                'Data length': 524288,
+                'Bytes/pixel': 2,
+                'Start context': 'OL',
+                'Data type': 'AFM',
+                'Note': None,
+                'Samps/line': 512,
+                'Number of lines': 512,
+                'Aspect ratio': '1:1',
+                'Line direction': 'Retrace',
+                'Highpass': 0,
+                'Lowpass': 0,
+                'Realtime planefit': 'Line',
+                'Offline planefit': 'Full',
+                'Valid data start X': 0,
+                'Valid data start Y': 0,
+                'Valid data len X': 512,
+                'Valid data len Y': 512,
+                'Tip x width correction factor': 1,
+                'Tip y width correction factor': 1,
+                'Tip x width correction factor sigma': 1,
+                'Tip y width correction factor sigma': 1,
+                'Z magnify': 0.4615211 ,
+                'Z scale': 0.2166748,
+                'Z offset': 0,
+            }
+        }
+        p = NanoscopeParser('./tests/files/header_multiple_images.txt', 'utf-8')
+        p.read_nanoscope()
+        self.assertDictEqual(data, p.config)
+        self.assertDictEqual(images, p.images)
