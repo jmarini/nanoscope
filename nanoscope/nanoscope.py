041360dee311fb33ca76f3f3f12524f128eb6e3b
Jonathan Marini <j.marini@ieee.org>
2014-10-25 00:21:14 -0400

    Fixed test discovery and added file to test parser.

--- nanoscope/nanoscope.py
+++ nanoscope/nanoscope.py
@@ -3,6 +3,8 @@ from __future__ import division, unicode_literals
 
 import numpy as np
 
+from .parameter import parse_parameter
+
 
 class NanoscopeParser(object):
     """
@@ -11,17 +13,66 @@ class NanoscopeParser(object):
 
     def __init__(self, filename):
         self.filename = filename
+        self.images = {}
+        self.config = {}
 
-    def _split_data(self, data, stride):
+    @property
+    def height(self):
         """
-        Split a 1d array into a 2d array
+        Return the height image if it exists, else ``None``.
         """
-        out = []
-        for n, value in enumerate(data):
-            if n % 512 == 0:
-                out.append([])
-            out[-1].append(value)
-        return out
+        return self.images.get('Height', None)
+
+    @property
+    def amplitude(self):
+        """
+        Return the amplitude image if it exists, else ``None``.
+        """
+        return self.images.get('Amplitude', None)
+
+    @property
+    def phase(self):
+        """
+        Return the phase image if it exists, else ``None``.
+        """
+        return self.images.get('Phase', None)
+
+    def read_nanoscope(self):
+        """
+        Read in the nanoscope file and convert the raw data.
+        """
+        self._read_header()
+        for image in self.images:
+            self._read_image_data(image)
+
+    def _read_header(self):
+        with open(self.filename, 'r') as f:
+            for line in f:
+                parameter = parse_parameter(line.rstrip('\n'))
+
+                if parameter.type == 'H':  # header
+                    if parameter.header == 'File list end':
+                        return
+                    elif parameter.header == 'Ciao image list':
+                        self._read_image_header(f)
+
+    def _read_image_header(self, f):
+        image_config = {}
+        position = f.tell()
+        for line in f:
+            parameter = parse_parameter(line.rstrip('\n'))
+            if parameter.type == 'H':
+                f.seek(position)
+                return
+            if parameter.type == 'S':
+                if parameter.param == 'Image Data':
+                    self.images[parameter.internal_designation] = image_config
+            else:
+                image_config[parameter.param] = parameter.hard_value
+            position = f.tell()
+
+    def _read_image_data(self, config):
+        pass
 
     def _flatten_scanline(self, data):
         a, b = np.polyfit(range(len(data)), data, 1)
---
+++ tests/test_parser.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from nanoscope.nanoscope import NanoscopeParser
+
+
+class TestNanoscopeParser(unittest.TestCase):
+    pass
