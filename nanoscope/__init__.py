3df4964b1ed6fddf421d0147f2ce4eeb3267f08e
Jonathan Marini <j.marini@ieee.org>
2015-02-10 15:48:31 -0500

    Added extra info to __init__.py

--- nanoscope/__init__.py
+++ nanoscope/__init__.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+    nanoscope
+    ---------
+
+    Library to parse and process Nanoscope Dimension AFM files.
+
+    :copyright: (c) 2015 by Jonathan Marini
+    :license: MIT, see LICENSE for details
+"""
+
+__version__ = '1.0.1'
+
 from .nanoscope import NanoscopeParser
